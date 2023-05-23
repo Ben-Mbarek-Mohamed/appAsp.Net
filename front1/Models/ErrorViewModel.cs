83e8bd1693a98a29c6f557c0f9234594dd11f443
Ben-Mbarek-Mohamed <115542032+Ben-Mbarek-Mohamed@users.noreply.github.com>
2023-05-23 03:15:50 +0100

    Delete front1 directory

--- front1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace front1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
